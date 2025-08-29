c9a394fa1960c6c654e178542792d32bcc3d9a33
Mahesh Rayate <Mahesh.Rayate@ril.com>
2025-08-29 12:37:50 +0530

    Updated login controller

--- app.js
+++ app.js
@@ -11,7 +11,12 @@ const app = express();
 // MIDDLEWARES
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // frontend URL
+    credentials: true, // allow cookies
+  })
+);
 
 app.get("/", (req, res) => {
   res.status(200).json({
