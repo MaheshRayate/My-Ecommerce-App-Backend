58efb83d619ab2877d607d6f6e7d8c66a59e23c8
Mahesh Rayate <98866046+MaheshRayate@users.noreply.github.com>
2025-08-29 19:07:26 +0530

    Update authControllers.js

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -68,14 +68,16 @@ exports.login = catchAsync(async (req, res, next) => {
 
 exports.protect = catchAsync(async (req, res, next) => {
   let token;
+  
+  if (req.cookies && req.cookies.jwt) {
+    token = req.cookies.jwt;
+  }
 
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
     token = req.headers.authorization.split(" ")[1];
-  } else if (req.cookies.jwt) {
-    token = req.cookies.jwt;
   }
 
   if (!token) {
