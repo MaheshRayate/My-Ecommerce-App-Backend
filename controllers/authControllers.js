8cf654ca48d42a51169f96514d6a686472d984eb
Mahesh Rayate <Mahesh.Rayate@ril.com>
2025-08-29 17:11:55 +0530

    Updated

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -9,7 +9,7 @@ const cookieOptions = {
     Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
   ),
   httpOnly: true, //for preventing cross site scripting
-  sameSite: "lax",
+  sameSite: "none",
   secure: true,
 };
 
